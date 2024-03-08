a83e89c758db6ae5f6e579f2565348e338a697ff
MichaelSukkiSong <sa77irak@gmail.com>
2024-03-07 17:41:01 -0800

    create an ai/rsc instsance on the server - initial

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,25 +1,25 @@
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+// import OpenAI from "openai";
+// import { OpenAIStream, StreamingTextResponse } from "ai";
 
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// // Create an OpenAI API client (that's edge friendly!)
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
-export async function POST(req: Request) {
-  const { messages } = await req.json();
+// export async function POST(req: Request) {
+//   const { messages } = await req.json();
 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    stream: true,
-    messages,
-  });
+//   // Ask OpenAI for a streaming chat completion given the prompt
+//   const response = await openai.chat.completions.create({
+//     model: "gpt-3.5-turbo",
+//     stream: true,
+//     messages,
+//   });
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Response with the stream
-  return new StreamingTextResponse(stream);
-}
+//   // Convert the response into a friendly text-stream
+//   const stream = OpenAIStream(response);
+//   // Response with the stream
+//   return new StreamingTextResponse(stream);
+// }
