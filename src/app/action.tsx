43ec8b5ef5a8cc99c6dd2eb454020893b041e779
MichaelSukkiSong <sa77irak@gmail.com>
2024-03-08 21:59:12 -0800

    add initial AI and UI state, create AI var

--- src/app/action.tsx
+++ src/app/action.tsx
@@ -33,4 +33,31 @@ function getFlightInfo(flightNumber: string) {
   };
 }
 
-async function submitUserMessage(userInput: string) {}
+async function submitUserMessage(userInput: string) {
+  const aiState = getMutableAIState<typeof AI>();
+}
+
+// Define the initial state of the AI. It can be any JSON object.
+const initialAIState: {
+  role: "user" | "assistant" | "system" | "function";
+  content: string;
+  id?: string;
+  name?: string;
+}[] = [];
+
+// The initial UI state that the client will keep track of, which contains the message IDs and their UI nodes.
+const initialUIState: {
+  id: number;
+  display: React.ReactNode;
+}[] = [];
+
+// AI is a provider you wrap your application with so you can access AI and UI state in your componets.
+export const AI = createAI({
+  actions: {
+    submitUserMessage,
+  },
+  // Each state can be any shape of object, but for chat applications
+  // it makes sense to have an array of messages. Or you may prefer something like { id: number, messages: Message[] }
+  initialUIState,
+  initialAIState,
+});
